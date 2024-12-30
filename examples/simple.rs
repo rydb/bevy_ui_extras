13a6fe3289cd241a82c34cd87863b7790383a1b6
rydb <ryan.dobe@gmail.com>
2024-12-30 17:39:50 -0600

    set back to top left

--- examples/simple.rs
+++ examples/simple.rs
@@ -19,7 +19,7 @@ fn main() {
         .register_type::<MeshInfoTarget>()
         .add_plugins(UiExtrasDebug {
             ui_style: UiStyle::BLACK_GLASS,
-            alignment: UiAlignment(Align2::CENTER_TOP),
+            alignment: UiAlignment(Align2::LEFT_TOP),
             menu_mode: DebugMenuState::Explain,
             ..default()
         })
